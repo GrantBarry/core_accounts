37ff5cca2e7825c0995db9c938a37b0b1c68479b
Grant Barry <grant.barry@gmail.com>
2024-05-25 15:49:09 +0200

    FIX: More model relation fixes

--- app/models/core/website.rb
+++ app/models/core/website.rb
@@ -6,5 +6,5 @@ class Core::Website < ApplicationRecord
   normalizes :enquiry_email, with: ->(attribute) { attribute.strip.downcase }
   normalizes :description, :title, :keywords, :enquiry_telephone, with: ->(attribute) { attribute.strip }
 
-  has_many :website_domains, dependent: :delete_all, class_name: 'Core::WebsiteDomain'
+  has_many :website_domains, dependent: :delete_all, class_name: 'Core::WebsiteDomain', foreign_key: :core_website_id
 end
--- lib/core_accounts/version.rb
+++ lib/core_accounts/version.rb
@@ -1,3 +1,3 @@
 module CoreAccounts
-  VERSION = "0.1.2"
+  VERSION = "0.1.4"
 end
